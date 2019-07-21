1433e90c2dd9826f47cdbaed9597f8383ffe89eb
Kirill Shevchenko <kirill_shevchenko1@epam.com>
2019-07-21 18:35:00 +0300

    +

---
+++ day1/useless/appserver.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+#
+# Installing appserver
+# Author: Kirill Shevchenko
+yum install -y  bind-utils traceroute telnet nmap iftop python-pip
+#
---
+++ day1/useless/webserver.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+#
+# Installing webserver
+# Author: Kirill Shevchenko
+yum install -y net-tools vim mc htop psmisc bind-utils traceroute telnet nmap iftop pip python-pip
+#pip install module_dependency
